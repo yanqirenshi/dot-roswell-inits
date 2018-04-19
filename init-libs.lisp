746719ec2c917477b06e3eeb72798cd2ff02dfc5
satoshi.iwasaki <satoshi.iwasaki@glpgs.com>
2018-04-19 11:27:25 +0900

    add glfw3

--- init-libs.lisp
+++ init-libs.lisp
@@ -1,4 +1,16 @@
+;;;;;
+;;;;; HTTP
+;;;;;
 (ql:quickload :dexador)
 (ql:quickload :woo)
 
+;;;;;
+;;;;; THREAD
+;;;;;
 (ql:quickload :bordeaux-threads)
+
+;;;;;
+;;;;; glfw3
+;;;;;
+(ql:quickload :cl-glfw3)
+(ql:quickload :cl-glfw3-examples)
